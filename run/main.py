0533d8fd92be943abb48f5dbd81edda025a689ed
Mikhail Kotyushev <mkotyushev@gmail.com>
2023-07-06 03:33:56 +0000

    Add torch.compile verbocity with DEBUG loglevel

--- run/main.py
+++ run/main.py
@@ -1,11 +1,17 @@
 import logging
 import os
+import torch
+import torch._dynamo
+
 
 LOGLEVEL = os.environ.get('LOGLEVEL', 'INFO').upper()
 logging.basicConfig(
     level=LOGLEVEL,
     format = '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s'
 )
+
+if LOGLEVEL == 'DEBUG':
+    torch._dynamo.config.verbose = True
 
 from src.utils.utils import MyLightningCLI, TrainerWandb
 
